9f727d37d2b6c91f162b92a26dae9deb4d433417
mdorochko23 <148977720+mdorochko23@users.noreply.github.com>
2024-03-12 14:11:13 -0400

    Add files via upload

---
+++ msgbox.h
@@ -0,0 +1,23 @@
+#DEFINE d_nokbutton 0
+#DEFINE d_nokcancel 1
+#DEFINE d_nabortretry 2
+#DEFINE d_nyesnocancel 3
+#DEFINE d_nyesno 4
+#DEFINE d_nretrycancel 5
+
+#DEFINE d_nstopsign 16
+#DEFINE d_nquestionmark 32
+#DEFINE d_nexclamation 48
+#DEFINE d_ninformation 64
+
+#DEFINE d_ndefaultfirst 0
+#DEFINE d_ndefaultsecond 256
+#DEFINE d_ndefaultthird 512
+
+#DEFINE d_nok 1
+#DEFINE d_ncancel 2
+#DEFINE d_nabort 3
+#DEFINE d_nretry 4
+#DEFINE d_nignore 5
+#DEFINE d_nyes 6
+#DEFINE d_nno 7
